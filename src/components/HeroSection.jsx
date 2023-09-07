7ecf7f5efa797ba9dd35e46b359c247225d8c99f
unknown <jakinaandriantsoa@gmail.com>
2023-09-07 16:57:32 +0300

    otrotrotrzay pr rangah fa tsy aiko ah

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-const Hero_image = require('../img/istockphoto-161838634-612x612.jpg');
+const Hero_image = require('../img/light-field-lab-concert-concept-art.jpeg');
 const Hip_amazon = require('../img/chariot.png');
 const Hip_ebay = require('../img/chariot.png');
 
@@ -7,7 +7,7 @@ function HeroSection() {
     return (
         <main className="hero container">
             <div className="hero-content">
-                <h1>LET US IN ROCK'N ROLL</h1>
+                <h1>LET US IN DA ROCK'N ROLL</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt repudiandae enim itaque exercitationem repellendus quas labore facilis dicta quod </p>
                 <div className="hero-btn">
                     <button>Shop now</button>
@@ -22,7 +22,7 @@ function HeroSection() {
                 </div>
             </div>
             <div className="Hero-image">
-                <img src={Hero_image} alt="Hero_image here" />
+                <img src={Hero_image} alt="Hero_image here" className='hi_hero' />
             </div>
         </main>
     )
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,23 @@
 const image = require('../img/jakina-inc-logo.png');
-function Navigation () {
+function Navigation() {
 
     return (
 
         <nav className="container">
             <div className='logo'>
-                <img src={image} alt="logo here" className='logo-image'/>
+                <img src={image} alt="logo here" className='logo-image' />
             </div>
             <ul>
+                <li href="#">Accueil</li>
                 <li href="#">Menu</li>
                 <li href="#">Evenement</li>
                 <li href="#">About</li>
                 <li href="#">Contact</li>
             </ul>
-
-            <button>Login</button>
+            <div className='class_btn'>
+                <button>Sign in</button>
+                <button className='secondary-btn'>Login</button>
+            </div>
         </nav>
     );
 };
